34b535d74c63716bdfee0817b9c1a2cf5919e3aa
gioneill <grego@gregorys-macbook-pro.local>
2016-11-19 15:58:31 -0500

    Added Xcode Project.

---
+++ my-curator-ios/my-curator-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-curator-ios
+//
+//  Created by Gregory O'Neill on 11/19/16.
+//  Copyright © 2016 Gregory O'Neill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
