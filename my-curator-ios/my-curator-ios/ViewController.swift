2b9e3402f1f133ead8900b3816873213e58e39e5
gioneill <grego@gregorys-macbook-pro.local>
2016-11-19 23:53:08 -0500

    Correctly ranging one beacon.

--- my-curator-ios/my-curator-ios/ViewController.swift
+++ my-curator-ios/my-curator-ios/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    let beaconManager = BeaconManager.sharedInstance
+    var beacons: [CLBeacon]!
+    var movingAverageThreshold: Int?
 
+    @IBOutlet weak var signalLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
+    var beetBeaconMovingAvg: Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //how many previous values to average
+        beaconManager.movingAverageThreshold = 3
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.beaconsUpdated), name: Notification.Name("beaconsManagerDidUpdateNotification"), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.authorizationDenied), name: Notification.Name("beaconsManagerAuthorizationDenied"), object: nil)
+        
+        
+        let transform = CGAffineTransform(scaleX: 5.0, y: 5.0)
+        activityIndicator.transform = transform
+        activityIndicator.startAnimating()
+    }
+    
+    func beaconsUpdated() {
+        beacons = beaconManager.beaconsArray
+        
+        for beacon in beacons {
+            if beacon.minor == 49339 {
+                if beacon.rssi != 0 {
+                    signalLabel.text = "Signal: \(beacon.rssi)"
+                }
+            }
+            
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func authorizationDenied() {
+        
     }
-
 
 }
 
